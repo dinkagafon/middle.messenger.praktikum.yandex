2950bf692786b2615249be6e9fcb04a3b0218a42
dinkagafon <agafonikita@outlook.com>
2021-10-23 12:11:46 +0300

    pre delete parcel

--- src/index.ts
+++ src/index.ts
@@ -49,3 +49,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   render('.root');
 });
+
+interface StepFn {
+  (): number;
+  (value: number): StepFn;
+}
+
+function add(val: number): StepFn;
+function add(): number;
+function add(val?: number): StepFn | number {
+  let sum = 0;
+  if (typeof val !== 'number') {
+    return sum;
+  }
+  sum += val;
+
+  function carr(b: number): StepFn;
+  function carr(): number;
+  function carr(b?: number): number | StepFn {
+    if (!b) {
+      return sum;
+    }
+    sum += b;
+    return carr;
+  }
+  return carr;
+}
+
+export default add;
+
+add(); // => 0
+add(1)(); // => 1
+add(1)(2)(); // => 3
